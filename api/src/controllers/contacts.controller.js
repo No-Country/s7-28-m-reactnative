c53f78dfc23ea85185be92e41204fd4d988ca80b
tomasherrera1910 <tomymza10@gmail.com>
2023-03-28 17:44:42 -0300

    add and delete controller

--- api/src/controllers/contacts.controller.js
+++ api/src/controllers/contacts.controller.js
@@ -1,1 +1,22 @@
+const { handlerHttp } = require('../utils/error.handler')
+const { addContact, deleteContact } = require('../services/contact.services')
 // En los controladores no va la logica del negocio, eso va en services
+const newContact = async (req, res) => {
+  const { userId, newContactId } = req.body
+  try {
+    const response = await addContact(userId, newContactId)
+    res.status(201).send(response)
+  } catch (error) {
+    handlerHttp(res, (!error ? 'Error adding contact' : error.message))
+  }
+}
+const removeContact = async (req, res) => {
+  const { userId, newContactId } = req.body
+  try {
+    const response = await deleteContact(userId, newContactId)
+    res.status(201).send(response)
+  } catch (error) {
+    handlerHttp(res, (!error ? 'Error removing contact' : error.message))
+  }
+}
+module.exports = { newContact, removeContact }
