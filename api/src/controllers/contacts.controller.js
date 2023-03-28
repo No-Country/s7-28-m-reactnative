8ff7c0b33c219505f799c85850d404d26fe780a4
tomasherrera1910 <tomymza10@gmail.com>
2023-03-28 18:08:17 -0300

    handling some possibles errors

--- api/src/controllers/contacts.controller.js
+++ api/src/controllers/contacts.controller.js
@@ -5,6 +5,12 @@ const newContact = async (req, res) => {
   const { userId, newContactId } = req.body
   try {
     const response = await addContact(userId, newContactId)
+    if (response === 'This user is already your contact') {
+      return res.status(406).json({ error: response })
+    }
+    if (response === 'User not found') {
+      return res.status(404).json({ error: response })
+    }
     res.status(201).send(response)
   } catch (error) {
     handlerHttp(res, (!error ? 'Error adding contact' : error.message))
@@ -14,7 +20,8 @@ const removeContact = async (req, res) => {
   const { userId, newContactId } = req.body
   try {
     const response = await deleteContact(userId, newContactId)
-    res.status(201).send(response)
+    if (response === 'Contact could not be deleted') { throw new Error(response) }
+    res.status(202).send(response)
   } catch (error) {
     handlerHttp(res, (!error ? 'Error removing contact' : error.message))
   }
