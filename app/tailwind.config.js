d7fae129e2ecdb978ab2e728b2a2b24058794e6d
Lucas <lucasabbona0@gmail.com>
2023-04-02 18:25:27 -0300

    Creating profile, edit profile, links between screens and some styling in FaQ page

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
     colors: {
       appred: '#C83D3D',
       appblue: '#4994C2',
-      appbluelight: '#E4EEF5',
-      appgray: '#E0E0E0',
+      appbluelight: '#C9D7E1',
+      appgray: '#8D8E92',
       applightgray: '#F4F4F4',
       appgreen: '#72B040',
       appwhite: '#FEFFFF',
       appblack: '#212121'
+    },
+    borderRadius: {
+      '4xl': '80px'
     }
   },
   plugins: []
