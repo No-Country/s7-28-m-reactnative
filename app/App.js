a114709ce63139ebfdd503c9699c684e01fe87b7
Lucas <lucasabbona0@gmail.com>
2023-04-03 10:50:03 -0300

    Applinks

--- app/App.js
+++ app/App.js
@@ -4,7 +4,8 @@ import LoginScreen from './src/screens/LoginScreen'
 import RegisterScreen from './src/screens/RegisterScreen'
 import RegisterSuccess from './src/screens/RegisterSuccess'
 import BottomNav from './src/BottomNav'
-
+import PersonalInfoScreen from './src/screens/PersonalInfoScreen'
+import FaQScreen from './src/screens/FaQScreen'
 const Stack = createNativeStackNavigator()
 
 export default function App () {
@@ -35,6 +36,20 @@ export default function App () {
         <Stack.Screen
           name='bottom'
           component={BottomNav}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name='personalinfoscreen'
+          component={PersonalInfoScreen}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name='frequentasked'
+          component={FaQScreen}
           options={{
             headerShown: false
           }}
