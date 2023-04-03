9ea8d871886567f9a2b9fa72b7d32a557a4e72f5
Raul Ereño <raulereno@gmail.com>
2023-04-03 10:20:38 -0300

    changue mongo url to env variable and delete prettierrc

--- api/src/config/mongo.js
+++ api/src/config/mongo.js
@@ -2,7 +2,7 @@ require('dotenv').config({})
 const { connect } = require('mongoose')
 
 async function dbConnect () {
-  const DB_URI = 'mongodb+srv://s7-28-admin:uhCTkDLnAplOklQI@cluster0.n4rem.mongodb.net/alwaysalert'
+  const DB_URI = process.env.DB_URI
   await connect(DB_URI)
 }
 module.exports = dbConnect
