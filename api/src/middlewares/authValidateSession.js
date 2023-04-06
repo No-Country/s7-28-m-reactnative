327599c1461f1e74b7215512c885a8e0802d9f8f
Raul Ereño <raulereno@gmail.com>
2023-04-06 11:59:25 -0300

    adding ternary to split in authorization

--- api/src/middlewares/authValidateSession.js
+++ api/src/middlewares/authValidateSession.js
@@ -3,7 +3,7 @@ const { verifyToken } = require('../utils/jwt.handle')
 const checkJwt = (req, res, next) => {
   try {
     const jwtByUser = req.headers.authorization || null
-    const jwt = jwtByUser.split(' ').pop()
+    const jwt = jwtByUser?.split(' ').pop()
     const isOk = verifyToken(jwt)
     if (!isOk) {
       res.status(401).send('INVALID_SESSION')
