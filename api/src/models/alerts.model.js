11696e6667de872f39feaefd9ab9903fdc97c3cf
Raul Ereño <raulereno@gmail.com>
2023-04-11 11:46:07 -0300

    formating dates

--- api/src/models/alerts.model.js
+++ api/src/models/alerts.model.js
@@ -1,11 +1,18 @@
 const { Schema, model } = require('mongoose')
+const { formatDate } = require('../utils/formatStrings')
 
 const AlertSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User' },
   ubication: { type: String },
-  reason: { type: String }
+  reason: { type: String },
+  time: { type: String, default: new Date() }
 }, {
-  timestamps: true, versionKey: false
+  timestamps: false, versionKey: false
+})
+
+AlertSchema.pre('save', function (next) {
+  this.time = formatDate()
+  next()
 })
 
 const AlertModel = model('Alert', AlertSchema)
