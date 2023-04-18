9a7efa372b83a222f06672713eb9f9dfbe1d6190
tomasherrera1910 <tomymza10@gmail.com>
2023-04-18 01:57:33 -0300

    add anchor for navigate to authors

--- always-alert-landing/src/components/Header/Navbar/index.jsx
+++ always-alert-landing/src/components/Header/Navbar/index.jsx
@@ -1,12 +1,14 @@
-import logo from '../../../../../../assets/logo.svg'
+import logo from '../../../../public/assets/logo.svg'
 
 export default function Navbar () {
   return (
-    <div className='flex justify-between px-60 py-10'>
+    <div className='flex justify-between w-full max-w-7xl px-60 py-10'>
       <img src={logo} alt='' srcset='' />
       <ul className='flex items-center'>
         {/* <li>Descargar</li> */}
-        <li>Nosotros</li>
+        <a href='#nosotros'>
+          <li className='font-bold text-xl ease-in-out duration-300 hover:underline'>Nosotros</li>
+        </a>
       </ul>
     </div>
   )
