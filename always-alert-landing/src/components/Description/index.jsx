f74278da85fc573ca80a3cb06fa5cc7ddddbfd5f
tomasherrera1910 <tomymza10@gmail.com>
2023-04-18 11:40:54 -0300

    description responsive

--- always-alert-landing/src/components/Description/index.jsx
+++ always-alert-landing/src/components/Description/index.jsx
@@ -1,25 +1,25 @@
 export default function Description () {
   return (
     <>
-      <section className='flex justify-center gap-[150px] my-20'>
-        <img width={311} src='/assets/description/phone.png' alt='Home' />
-        <div className='w-[657px] h-[671px] rounded-full flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-transparent'>
-          <h3 className='w-2/4 text-center text-xl'>
+      <section className='flex flex-col lg:flex-row justify-center lg:mx-8 items-center gap-36 my-12 sm:my-20'>
+        <img className='w-64 sm:w-auto' src='/assets/description/phone.png' alt='Home' />
+        <div className='lg:w-[640px] lg:h-[640px] rounded-full flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-transparent'>
+          <h3 className='w-2/4 text-center text-xl sm:text-2xl'>
             Ante cualquier <b>accidente o evento inesperado </b> contaras con nosotros
             para avisar a tus contactos.
           </h3>
-          <img width={357} src='/assets/description/character.png' alt='' />
+          <img className='w-64 object-cover sm:w-3/4' src='/assets/description/character.png' alt='' />
         </div>
       </section>
-      <section className='h-full flex justify-center items-center gap-[150px] my-40'>
-        <div className='w-[657px] h-[671px] rounded-full flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-transparent'>
-          <h3 className='w-2/4 text-center text-xl'>
+      <section className='h-full flex flex-col lg:flex-row justify-center lg:mx-8 items-center gap-12 sm:gap-36 my-12 sm:my-20'>
+        <div className='lg:w-[640px] lg:h-[640px] rounded-full flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-transparent'>
+          <h3 className='w-2/4 text-center text-xl sm:text-2xl'>
             <b>Nuestra app te ayuda</b> a infórmarle a tus contactos tu estado,
             para que estén siempre alertas.
           </h3>
-          <img width={357} src='/assets/description/character2.png' alt='' />
+          <img className='w-64 object-cover sm:w-3/4' src='/assets/description/character2.png' alt='' />
         </div>
-        <img width={311} src='/assets/description/phone2.png' alt='Home' />
+        <img className='w-64 sm:w-auto' src='/assets/description/phone2.png' alt='Home' />
       </section>
     </>
   )
