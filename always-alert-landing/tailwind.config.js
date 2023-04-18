02c407f1396d090279364342caed0a3c3c10df5b
tomasherrera1910 <tomymza10@gmail.com>
2023-04-18 11:39:15 -0300

    comment tailwind config

--- always-alert-landing/tailwind.config.js
+++ always-alert-landing/tailwind.config.js
@@ -8,26 +8,26 @@ export default {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif']
-      },
-      screens: {
-        '2xl': { max: '1535px' },
-        // => @media (max-width: 1535px) { ... }
-
-        xl: { max: '1279px' },
-        // => @media (max-width: 1279px) { ... }
-
-        lg: { max: '1023px' },
-        // => @media (max-width: 1023px) { ... }
-
-        md: { max: '767px' },
-        // => @media (max-width: 767px) { ... }
-
-        sm: { max: '639px' },
-        // => @media (max-width: 639px) { ... }
-
-        xs: { max: '479px' }
-        // => @media (max-width: 479px) { ... }
       }
+      // screens: {
+      //   '2xl': { min: '1535px' },
+      //   // => @media (max-width: 1535px) { ... }
+
+      //   xl: { min: '1279px' },
+      //   // => @media (max-width: 1279px) { ... }
+
+      //   lg: { min: '1023px' },
+      //   // => @media (max-width: 1023px) { ... }
+
+      //   md: { min: '767px' },
+      //   // => @media (max-width: 767px) { ... }
+
+      //   sm: { min: '639px' },
+      //   // => @media (max-width: 639px) { ... }
+
+      //   xs: { min: '479px' }
+      //   // => @media (max-width: 479px) { ... }
+      // }
     }
   },
   plugins: []
