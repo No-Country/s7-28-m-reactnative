5e7b900b2f6ec8fb20a3acd1013a16ee0df23e3f
tomasherrera1910 <tomymza10@gmail.com>
2023-04-18 14:18:19 -0300

    fixing the responsive to raul's taste

--- always-alert-landing/src/components/Authors/index.jsx
+++ always-alert-landing/src/components/Authors/index.jsx
@@ -52,12 +52,12 @@ const authors = [
 
 export default function Authors () {
   return (
-    <section className='h-full bg-[#C9D7E1] flex flex-col items-center py-20 rounded-tl-[80px] rounded-tr-[80px]' id='nosotros'>
+    <section className='h-full bg-[#C9D7E1] flex flex-col items-center py-20 px-8 sm:px-0 rounded-tl-[80px] rounded-tr-[80px]' id='nosotros'>
       <h2 className='w-full max-w-xl px-5 text-center text-xl sm:text-2xl'>
         Somos un equipo multidisciplinario de <b>Programadores y Diseñadoras </b> que
         realizamos una aplicación completamente funcional en 5 semanas.
       </h2>
-      <section className='flex flex-wrap max-w-5xl gap-12 justify-center mt-10'>
+      <section className='flex flex-col sm:flex-row sm:flex-wrap max-w-5xl gap-12 justify-center mt-10'>
         {authors.map((author) => {
           return (
             <AuthorCard key={author.name} author={author} />
--- always-alert-landing/src/components/Footer/index.jsx
+++ always-alert-landing/src/components/Footer/index.jsx
@@ -2,21 +2,19 @@ import DonwloadButton from '../DownloadButton'
 
 export default function Footer () {
   return (
-    <footer className='h-full bg-[#212121] flex flex-col gap-10 items-center py-10'>
+    <footer className='h-full bg-[#212121] flex flex-col gap-10 items-center py-10 relative'>
       <div className='flex flex-col gap-8 sm:gap-20 items-center w-full max-w-6xl'>
         <h2 className='text-white text-3xl sm:text-5xl text-center'>
           ¿Listo para ser parte de Always Alert?
         </h2>
-        <section className='flex flex-col gap-10'>
+        <section className='flex flex-col gap-10 items-center'>
           <DonwloadButton />
-          <img className='w-[332.14px]' src='/assets/footer/character.png' alt='' />
+          <img className='w-96' src='/assets/footer/character.png' alt='' />
         </section>
-        <div className='flex items-center'>
-          <img className='hidden sm:block sm:w-24 p-4' src='/assets/footer/white_logo.png' alt='' />
-          <p className='grow text-white text-xl text-center'>
-            Todos los derechos reservados para el equipo S7-28-M
-          </p>
-        </div>
+        <p className='text-white text-xl text-center'>
+          Todos los derechos reservados para el equipo S7-28-M
+        </p>
+        <img className='hidden md:block absolute bottom-5 left-5' src='/assets/footer/white_logo.png' alt='' />
       </div>
     </footer>
   )
