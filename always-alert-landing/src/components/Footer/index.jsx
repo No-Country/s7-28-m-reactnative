651e8496efd7e101d139dc5a08c965698a05b2cc
tomasherrera1910 <tomymza10@gmail.com>
2023-04-18 11:41:00 -0300

    footer responsive

--- always-alert-landing/src/components/Footer/index.jsx
+++ always-alert-landing/src/components/Footer/index.jsx
@@ -3,18 +3,17 @@ import DonwloadButton from '../DownloadButton'
 export default function Footer () {
   return (
     <footer className='h-full bg-[#212121] flex flex-col gap-10 items-center py-10'>
-      <div className='flex flex-col gap-20 items-center w-full max-w-6xl relative'>
-        <h2 className='text-white text-5xl'>
+      <div className='flex flex-col gap-8 sm:gap-20 items-center w-full max-w-6xl'>
+        <h2 className='text-white text-3xl sm:text-5xl text-center'>
           ¿Listo para ser parte de Always Alert?
         </h2>
         <section className='flex flex-col gap-10'>
           <DonwloadButton />
           <img className='w-[332.14px]' src='/assets/footer/character.png' alt='' />
         </section>
-        <div className='flex'>
-          <img width={80} className='absolute bottom-6 left-0' src='/assets/footer/white_logo.png' alt='' />
-
-          <p className='text-white text-xl'>
+        <div className='flex items-center'>
+          <img className='hidden sm:block sm:w-24 p-4' src='/assets/footer/white_logo.png' alt='' />
+          <p className='grow text-white text-xl text-center'>
             Todos los derechos reservados para el equipo S7-28-M
           </p>
         </div>
