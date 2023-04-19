78df39477e34fd1c17dad7c686c7780d8998047d
tomasherrera1910 <tomymza10@gmail.com>
2023-04-18 23:39:39 -0300

    fix modal z-index

--- always-alert-landing/src/components/DownloadModal/index.jsx
+++ always-alert-landing/src/components/DownloadModal/index.jsx
@@ -3,7 +3,7 @@ export default function DownloadModal ({ open, handleClose }) {
   const baseButtonStyle = 'gap-3 py-2 px-2 lg:py-4 sm:px-4 md:px-8 lg:px-12 rounded-full font-inter text-white mt-2 duration-300 ease-in-out hover:scale-110'
   if (open) {
     return (
-      <div className='h-screen w-screen fixed top-0 left-0 bg-black bg-opacity-80 flex justify-center items-center'>
+      <div className='z-10 h-screen w-screen fixed top-0 left-0 bg-black bg-opacity-80 flex justify-center items-center'>
         <dialog open={open} className='bg-gray-300 rounded-md flex flex-col gap-8 w-11/12 sm:max-w-md sm:gap-16 p-8'>
           <h3 className='text-xl text-center sm:text-2xl'>¿Quieres descargar <b>Always Alert</b>?</h3>
           <section className='w-full flex justify-around'>
